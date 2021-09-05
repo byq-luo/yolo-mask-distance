cc1814cdd0153b89e2feca900857c065c82f7a1c
chienyl <12345zox@gmail.com>
2021-09-05 20:28:49 +0800

    9.5

---
+++ mp.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import random
+import numpy
+import time
+
+def worker(q):
+    #plt.ion()
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ln, = ax.plot([], [])
+    fig.canvas.draw()   # draw and show it
+    plt.show(block=False)
+
+    while True:
+        obj = q.get()
+        n = obj + 0
+        print ("sub : got:", n)
+
+        ln.set_xdata(numpy.append(ln.get_xdata(), n))
+        ln.set_ydata(numpy.append(ln.get_ydata(), n))
+        ax.relim()
+
+        ax.autoscale_view(True,True,True)
+        fig.canvas.draw()
+
+if __name__ == '__main__':
+    queue = mp.Queue()
+    p = mp.Process(target=worker, args=(queue,))
+    p.start()
+
+    while True:
+        n = random.random() * 5
+        print("main: put:", n)
+        queue.put(n)
+        time.sleep(1.0)
